b91a052a053a785761ecbf85e2586e921b78a462
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-08-29 20:27:01 +0200

    czas twania

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const fs = require("fs");
 const path = require("path");
 
+
+
+
 console.log("Loguje przed odczytaniem");
+
+console.time("Czas trwania: ");
 
 fs.readFile(path.join(__dirname, "data", "lorem.txt"), function () {
   console.log("Odczytałem plik");
+  console.timeEnd("Czas trwania: ");
 });
 console.log("Loguje po odczytaniu");
