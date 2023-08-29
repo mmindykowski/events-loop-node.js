349b03b540f3ff9aa57f45f49b5949f3e6145f2e
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-08-29 20:29:54 +0200

    koniec cwiczenia

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const fs = require("fs");
 const path = require("path");
 
+function sleep(ms) {
+  let now = Date.now();
 
-
+  while (Date.now() - now < ms) {}
+}
 
 console.log("Loguje przed odczytaniem");
 
@@ -12,4 +15,6 @@ fs.readFile(path.join(__dirname, "data", "lorem.txt"), function () {
   console.log("Odczytałem plik");
   console.timeEnd("Czas trwania: ");
 });
+
+sleep(6000);
 console.log("Loguje po odczytaniu");
